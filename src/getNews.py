020d0bf1beb447296fd2301a8ec3317047b66a71
yeon0729 <yeon0729@sookmyung.ac.kr>
2024-08-29 21:12:36 +0900

    Feat: get title, description, url of articles

--- src/getNews.py
+++ src/getNews.py
@@ -7,4 +7,13 @@ load_dotenv(verbose=True)
 NEWS_TOKEN = os.getenv("NewsAPI")
 newsapi = NewsApiClient(api_key=NEWS_TOKEN)
 
-print(NEWS_TOKEN)
+# get top headlines of bbc-news
+top_headlines = newsapi.get_top_headlines(sources='bbc-news', page_size=5)
+
+# get title, description, url of articles
+for idx, article in enumerate(top_headlines['articles'], start=1):
+    print(f"Article {idx}:")
+    print(f"Title: {article['title']}")
+    print(f"Description: {article['description']}")
+    print(f"URL: {article['url']}")
+    print("-" * 80)
