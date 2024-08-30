0fadd9fc88abad557179304d6f0b21bac3da1e3f
yeon0729 <yeon0729@sookmyung.ac.kr>
2024-08-30 21:50:32 +0900

    Feat: create response from openai api using gpt model

--- src/getNews.py
+++ src/getNews.py
@@ -3,9 +3,10 @@ import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from newsapi import NewsApiClient
-import openai
+from openai import OpenAI
 
 load_dotenv(verbose=True)
+client = OpenAI(api_key=os.getenv("openaiAPI"))
 
 def get_news_urls(n: int=30):
     NEWS_TOKEN = os.getenv("NewsAPI")
@@ -22,9 +23,8 @@ def get_news_urls(n: int=30):
         print(f"URL: {article['url']}")
         print("-" * 80)
         
-    # 기사 URL 목록 추출
-    article_urls = [article['url'] for article in top_headlines['articles']]
-        
+    # extract url from article lists
+    article_urls = [article['url'] for article in top_headlines['articles']]  
     return article_urls
     
     
@@ -38,14 +38,30 @@ def get_article_txt_from_url(url):
     
     # join all text to one string
     article_text = '\n'.join([para.get_text() for para in paragraphs])
-    
     return article_text
 
 
 def convert_txt_to_steps(context: str, level: str):
     
-    openai.api_key = os.getenv("openaiAPI")
+    # set prompt for gpt model
+    prompt = f"Rewrite the following article at a {level} level: {context}"
     
+    # get response form gpt model
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant that rewrites articles."},
+            {"role": "user", "content": prompt}
+        ],
+        max_tokens=2048,
+        temperature=0.5
+    )
+    
+    # return response text
+    response_dict = response.model_dump()
+    response_message = response_dict["choices"][0]["message"]["content"]
+    return response_message
+
     
 if __name__ == "__main__":
     article_urls = get_news_urls(n=1)
@@ -56,9 +72,13 @@ if __name__ == "__main__":
         print(f"Article {idx} text: \n{article_txt[:500]}...")
         print("-" * 80)
         
-        proficiency_level = ["elementary school", "middle/high school", "university"]
+        proficiency_level = ["elementary school", "middle-high school", "university"]
         for level in proficiency_level:
             converted_txt = convert_txt_to_steps(context=article_txt, level=level)
             print(f"Article {idx} text: \n{converted_txt[:500]}...")  
+            with open(f"{level}.txt", "w", encoding="utf-8") as file:
+                file.write(converted_txt)
+                print("File is successfully saved ... !")
             print("o" * 20)
-        
+            
+            
