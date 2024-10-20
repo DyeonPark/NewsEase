0e4d5aa6c33ad5020e2c461f06881f64100c6aa5
yeon0729 <yeon0729@sookmyung.ac.kr>
2024-10-20 16:38:17 +0900

    Feat: get metainfo using newsapi

--- src/back/getNews.py
+++ src/back/getNews.py
@@ -10,20 +10,28 @@ load_dotenv(verbose=True)
 client = OpenAI(api_key=os.getenv("openaiAPI"))
 
 
-def get_news_urls_from_bbc(n: int = 5) -> List[str]:
+def get_news_metainfo_from_bbc(n: int = 5) -> List[str]:
     # set tokens for NewsAPI
     NEWS_TOKEN = os.getenv("NewsAPI")
     newsapi = NewsApiClient(api_key=NEWS_TOKEN)
     
     # get top headlines of bbc-news
     top_headlines = newsapi.get_top_headlines(sources='bbc-news', page_size=n)
+    
+    # extract meta information from article lists
+    articles = []
+    for article in top_headlines['articles']:
+        info_dict = dict()
+        info_dict["title"] = article['title']
+        info_dict["description"] = article['description']
+        info_dict["url"] = article['url']
+        info_dict["urlToImage"] = article['urlToImage']
+        articles.append(info_dict)
         
-    # extract url from article lists
-    article_urls = [article['url'] for article in top_headlines['articles']]  
-    return article_urls
+    return articles
     
     
-def get_article_n_img_from_url(url: str) -> Tuple[str, str]:
+def get_article_from_url(url: str) -> str:
     # get HTML content of url
     print(url)
     response = requests.get(url)
@@ -33,23 +41,4 @@ def get_article_n_img_from_url(url: str) -> Tuple[str, str]:
     paragraphs = soup.find_all('p')
     article_text = '\n'.join([para.get_text() for para in paragraphs])
     
-    # find div tag with "image-block" property
-    image_block_div = soup.find('div', {'data-component': 'image-block'})
-    img_tag = image_block_div.find("img", {"srcset": True})
-    
-    srcset = img_tag.get("srcset")
-    urls = [url.strip() for url in srcset.split(',')]
-    
-    url_480w = None
-    for url in urls:
-        if '480w' in url:
-            # catch url infront of '480w'
-            url_480w = url.split(' ')[0]
-            break
-
-    if url_480w:
-        print("Extracted Image URL:", url_480w)
-    else:
-        print("No URL with 480w found.")
-    
-    return article_text, url_480w
+    return article_text
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from back.getNews import get_news_urls_from_bbc, get_article_n_img_from_url
+from back.getNews import get_news_metainfo_from_bbc, get_article_from_url
 from back.createTTS import create_tts_from_txt
 from back.createNews import convert_txt_to_steps
 
@@ -28,68 +28,73 @@ if __name__ == "__main__":
     # get article urls from NewsAPI
     data_path = "tmp-data"
     N = 1
-    urls = get_news_urls_from_bbc(n=N) 
+    articles = get_news_metainfo_from_bbc(n=N) 
     
     # get texts and image urls from news url
-    for idx, url in enumerate(urls):
+    for idx, item in enumerate(articles):
         # set directory for temp data save
         now_date_n_num =f"{str(datetime.now().strftime("%Y-%m-%d"))}-{idx}"
         save_dir = os.path.join(data_path, now_date_n_num)
         check_n_crate_folder(os.path.join(save_dir))
         
+        with open(os.path.join(save_dir, "title.txt"), "w", encoding="utf-8") as file:
+            file.write(item["title"])
+            
+        with open(os.path.join(save_dir, "abstract.txt"), "w", encoding="utf-8") as file:
+            file.write(item["description"])
+            
+        with open(os.path.join(save_dir, "img-url.txt"), "w", encoding="utf-8") as file:
+            file.write(item["urlToImage"])
+        
         with open(os.path.join(save_dir, "url.txt"), "w", encoding="utf-8") as file:
-            file.write(url)
+            file.write(item["url"])
         
         # get article and image url
-        article, img_url = get_article_n_img_from_url(url)
+        article = get_article_from_url(item["url"])
         
         if article:
             with open(os.path.join(save_dir, "article.txt"), "w", encoding="utf-8") as file:
                 file.write(article)
         
-        if img_url:
-            with open(os.path.join(save_dir, "img-url.txt"), "w", encoding="utf-8") as file:
-                file.write(img_url)
+    # # base variable for checking today directory
+    # now_date = str(datetime.now().strftime("%Y-%m-%d"))
+    # dir_list = os.listdir(data_path)
+    # news_level = {
+    #     1: "elementary school",
+    #     2: "middle and high school",
+    #     3: "university"
+    # }
     
-    # base variable for checking today directory
-    now_date = str(datetime.now().strftime("%Y-%m-%d"))
-    dir_list = os.listdir(data_path)
-    news_level = {
-        1: "elementary school",
-        2: "middle and high school",
-        3: "university"
-    }
-    
-    # create article with levels in tmp-data folder
-    for dir in dir_list:
-        if now_date in dir:
-            origin_path = os.path.join(data_path, dir)
-            print(f">>> 기사 생성 작업 디렉토리 : {origin_path}")
+    # # create article with levels in tmp-data folder
+    # for dir in dir_list:
+    #     if now_date in dir:
+    #         origin_path = os.path.join(data_path, dir)
+    #         print(f">>> 기사 생성 작업 디렉토리 : {origin_path}")
             
-            with open(os.path.join(origin_path, 'article.txt'), 'r', encoding='utf-8') as file:
-                file_content = file.read()
+    #         with open(os.path.join(origin_path, 'article.txt'), 'r', encoding='utf-8') as file:
+    #             file_content = file.read()
                 
-                for level in [1, 2, 3]:
-                    leveled_text = convert_txt_to_steps(context=file_content, level=news_level[level])
-                    print(f">>> >>> 재생성 진행중인 기사 파일: {os.path.join(origin_path, f"article-{level}.txt")}")
+    #             for level in [1, 2, 3]:
+    #                 leveled_text = convert_txt_to_steps(context=file_content, level=news_level[level])
+    #                 print(f">>> >>> 재생성 진행중인 기사 파일: {os.path.join(origin_path, f"article-{level}.txt")}")
                     
-                    with open(os.path.join(origin_path, f"article-{level}.txt"), "w", encoding="utf-8") as file:
-                        file.write(leveled_text)
+    #                 with open(os.path.join(origin_path, f"article-{level}.txt"), "w", encoding="utf-8") as file:
+    #                     file.write(leveled_text)
                     
             
-    # create tts file of today
-    for dir in dir_list:
-        if now_date in dir:
-            tts_path = os.path.join(data_path, dir)
-            print(f">>> 작업 디렉토리 : {tts_path}")
+    # # create tts file of today
+    # for dir in dir_list:
+    #     if now_date in dir:
+    #         tts_path = os.path.join(data_path, dir)
+    #         print(f">>> 작업 디렉토리 : {tts_path}")
             
-            file_list = os.listdir(tts_path)
-            article_list = [file for file in file_list if file.startswith('article-') and file.endswith('.txt')]
-            for article in article_list:
-                article_path = os.path.join(tts_path, article)
-                print(f">>> >>> TTS 파일 생성중: {article_path}")
-                create_tts_from_txt(article_path)
-                print(f">>> >>> TTS 파일 생성 완료 !! {article_path}")
+    #         file_list = os.listdir(tts_path)
+    #         article_list = [file for file in file_list if file.startswith('article-') and file.endswith('.txt')]
+    #         for article in article_list:
+    #             article_path = os.path.join(tts_path, article)
+    #             print(f">>> >>> TTS 파일 생성중: {article_path}")
+    #             create_tts_from_txt(article_path)
+    #             print(f">>> >>> TTS 파일 생성 완료 !! {article_path}")
     
-    # post all data to Anvil NewsEase
+    # # post all data to Anvil NewsEase
     
