609479d27a515759e2961e0e2abc7a3650d49db8
yeon0729 <yeon0729@sookmyung.ac.kr>
2024-12-15 23:25:42 +0900

    Feat: update api path

--- src/api/constant.py
+++ src/api/constant.py
@@ -1,6 +1,13 @@
 # Anvil API 엔드포인트 URL
 API_BASE = "https://news-ease.com/_/api/"
-ADD_ARTICLE_API = API_BASE + "add_article"
-ADD_AUDIO_API = API_BASE + "add_audio"
+
+ADD_ARTICLE_META_API = API_BASE + "add_article_meta"
+ADD_ARTICLE_LEVEL_API = API_BASE + "add_article_level"
+ADD_AUDIO_API = API_BASE + "add_article_audio"
+ADD_WORD_API = API_BASE + "add_article_words"
+
+CHECK_WORD_EXIST = API_BASE + "existed_word"
+GET_MAX_WORD_ID = API_BASE + "max_word_id"
 GET_MAX_ID_API = API_BASE + "max_id"
+
 GET_DAILY_VISITS = API_BASE + "daily"
